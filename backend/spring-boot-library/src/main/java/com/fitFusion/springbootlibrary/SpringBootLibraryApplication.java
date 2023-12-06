3ded3b2e06ab6140f742a67c7b8af9c0a1ddd500
chawla-si <chawla.si@northeastern.edu>
2023-12-05 20:48:55 -0500

    Fix default port for UI

--- backend/spring-boot-library/src/main/java/com/fitFusion/springbootlibrary/SpringBootLibraryApplication.java
+++ backend/spring-boot-library/src/main/java/com/fitFusion/springbootlibrary/SpringBootLibraryApplication.java
@@ -32,7 +32,7 @@ public class SpringBootLibraryApplication {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:5174");
+                registry.addMapping("/**").allowedOrigins("http://localhost:5173");
             }
         };
     }
--- backend/spring-boot-library/src/main/java/com/fitFusion/springbootlibrary/controller/ExerciseController.java
+++ backend/spring-boot-library/src/main/java/com/fitFusion/springbootlibrary/controller/ExerciseController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@CrossOrigin( origins = "http://localhost:5174")
+@CrossOrigin( origins = "http://localhost:5173")
 @RestController
 public class ExerciseController {
 
